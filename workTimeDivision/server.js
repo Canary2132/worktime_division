7b1d81c6170f470675594626bac68186f1e3c44a
VolodymyrKovalenko <32463391+VolodymyrKovalenko@users.noreply.github.com>
2018-07-06 23:40:11 +0300

    i delete all blank lines that server.js needs

--- workTimeDivision/server.js
+++ workTimeDivision/server.js
@@ -3,14 +3,12 @@ const mongo = require('mongoose');
 const path = require('path');
 const api = require('./server/api.js');
 const bodyParser = require('body-parser');
-
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use('/api', api);
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
