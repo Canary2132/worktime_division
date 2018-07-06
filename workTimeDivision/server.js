d5f55accc643143f69431982f4857742d5374ce2
VolodymyrKovalenko <32463391+VolodymyrKovalenko@users.noreply.github.com>
2018-07-06 23:20:32 +0300

    remove some blank lines

--- workTimeDivision/server.js
+++ workTimeDivision/server.js
@@ -9,9 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'dist')));
-
 app.use('/api', api);
-
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
